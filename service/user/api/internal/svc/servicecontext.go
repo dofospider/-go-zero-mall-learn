9d70665caab924359fc8b2c2d6031020a6e20f18
dofospider <dofospider@gmail.com>
2022-09-30 23:17:40 +0800

    runed command goctl api go -api ./api/user.api -dir ./api

---
+++ service/user/api/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"go-zero-mall-learn/service/user/api/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
